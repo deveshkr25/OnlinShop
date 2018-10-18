10143a4a526e13f45a593d2aeb43ee3e4f8410fb
devesh <deveshkr25@gmail.com>
2018-10-18 19:08:03 +0530

    boostrap framework added in project

--- Shoping/src/main/java/com/dev/shop/Shoping/controller/PageContoller.java
+++ Shoping/src/main/java/com/dev/shop/Shoping/controller/PageContoller.java
@@ -1,6 +1,7 @@
 package com.dev.shop.Shoping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,9 +18,10 @@ public class PageContoller {
 		return mv;
 		
 	}
-	@RequestMapping(value="/test")
-	public ModelAndView test(@RequestParam(value="msg",required=false) String msg){
+	@RequestMapping(value="/test/{msg}")
+	//public ModelAndView test(@RequestParam(value="msg",required=false) String msg){
 		
+		public ModelAndView test(@PathVariable(value="msg",required=false) String msg){
 		if(msg == null){
 		   msg="hello test";	
 		}
